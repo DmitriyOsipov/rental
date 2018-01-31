58ac7ced1e79e790937fc1394ac4be0d88af1edb
DmitriyOsipov <osipov.dmal@gmail.com>
2018-01-31 16:15:16 +0200

    project configured for jsp

---
+++ src/main/java/com/carrental/controller/RootController.java
@@ -0,0 +1,15 @@
+package com.carrental.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/")
+public class RootController {
+
+  @RequestMapping(method = RequestMethod.GET)
+  public String initController() {
+    return "index";
+  }
+}
